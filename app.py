77c6dbe0d923d8a1b717417b583150dd621ada73
noelnamai <@RiverFront$2016>
2020-05-10 07:37:28 -0400

    update

--- app.py
+++ app.py
@@ -1,45 +1,12 @@
 import mysql.connector as mysql
-from flask import Flask, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/api/genes", methods=["GET", "POST"])
-def collection():
-    if request.method == "GET":
-        pass  #handle GET all Request
-    elif request.method == "POST":
-        pass  #handle POST request
-
-
-@app.route("/api/samples", methods=["GET", "POST"])
-def collection():
-    if request.method == "GET":
-        pass  #handle GET all Request
-    elif request.method == "POST":
-        pass  #handle POST request
-
-
-@app.route("/api/gene/<hgnc_name>", methods=["GET", "PUT", "DELETE"])
-def resource(hgnc_name):
-    if request.method == "GET":
-        pass  #handle GET single request
-    elif request.method == "PUT":
-        pass  #handle UPDATE request
-    elif request.method == "DELETE":
-        pass  #handle DELETE request
-
-
-@app.route("/api/sample/<sample_id>", methods=["GET", "PUT", "DELETE"])
-def resource(sample_id):
-    if request.method == "GET":
-        pass  #handle GET single request
-    elif request.method == "PUT":
-        pass  #handle UPDATE request
-    elif request.method == "DELETE":
-        pass  #handle DELETE request
-
+@app.route('/')
+def hello_world(name=None):
+    return render_template('index.html', name=name)
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+    app.run(debug = True)
